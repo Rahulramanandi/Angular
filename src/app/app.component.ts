82fad1b68deaf1adae3caf62f8ed1c44fb207195
Rahulramanandi <100043600+Rahulramanandi@users.noreply.github.com>
2025-03-04 14:56:42 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Basics';
+  description = 'This is a simple Angular application to demonstrate components and data binding.';
+}
